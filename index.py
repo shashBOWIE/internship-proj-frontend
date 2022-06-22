66482cb2e338210c57c398589a2a57a3d0131d63
shashBOWIE <92175840+shashBOWIE@users.noreply.github.com>
2022-06-22 11:32:51 -0500

    Add files via upload

--- index.py
+++ index.py
@@ -5,7 +5,6 @@ question = [1,11,13,17]
 #West
 NorthwestHillsArr = [];
 CatMountainArr = [];
-WestVeiwArr = [];
 HighlandParkArr = [];
 TarryTownArr = [];
 RollingWoodArr = [];
@@ -15,12 +14,7 @@ BartonCreekArr = [];
 RobRoyArr = [];
 AustinLakeArr = [];
 SteinerRanchArr = [];
-SennaHillsArr = [];
-LakePointeArr = [];
-LakewayArr = [];
-HudsonBendArr = [];
 RiverPlaceArr = [];
-ComancheTrailArr = [];
 JesterEstatesArr = [];
 
 #SouthWest
@@ -36,31 +30,17 @@ BouldinCreekArr = [];
 SouthCongressArr = [];
 TravisHeightsArr = [];
 StEdwardsArr = [];
-DawsonArr = [];
-GalindoArr = [];
 SouthLamarArr = [];
-WestgateArr = [];
-SiuthManchacaArr = [];
 WestCongressArr = [];
 BattleBendArr = [];
-SweetBriarArr = [];
-GarrisonParkArr = [];
-CherryCreekArr = [];
-ShadyHollowArr = [];
 
 #SouthEast
 
-RiversideArr = [];
-PleasantValleyArr = [];
-MontopolisArr = [];
-ParkerLaneArr = [];
 McKinneyArr = [];
-FranklinParkArr = [];
 
 #Central
 OldWestAustinArr = [];
 OldEnFieldArr = [];
-WindsorRoadArr = [];
 BrykerWoodsArr = [];
 RosedaleArr = [];
 TriangleStateArr = [];
@@ -70,39 +50,28 @@ UniOfTexArr = [];
 DownTownArr = [];
 
 #Northwest
-GrandviewHillsArr = []
-CanyonCreekArr = []
-AndersonMillArr = []
+
 GreatHillsArr = []
 BalconesWoodsArr = []
-AngusRanchArr = []
 AveryRanchArr = []
 
 #North
+
 AllandaleArr = []
-BrentwoodArr = []
 NorthLoopArr = []
-HighlandArr = []
 CrestviewArr = []
-NorthShoalCreekArr = []
-WootenArr = []
 NorthBurnetArr = []
-NorthAustinCivicAssociationArr = []
-GeorgianAcresArr = []
-NorthLamarArr = []
 GracyWoodsArr = []
-LamplightVillageArr = []
+
 
 #Northeast
-WindsorParkArr = []
-PecanSprings_SpringdaleArr = []
-UniversityHillsArr = []
-CoronadoHillsArr = []
-StJohnsArr = []
-HeritageHillsArr = []
+
 WindsorHillsArr = []
-CopperfieldArr = []
+
 
 if question[0] == 1:
-    res = sum(x == y for x, y in zip(question, NorthwestHillsArr))
-    print("Summation of Identical elements : " + str(res))
+    res = sum(x == y for x, y in zip(question, test_list2))
+    counting = int(str(res))
+    print(counting)
+    if counting > 2:
+        print("hi")
